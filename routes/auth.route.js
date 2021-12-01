d82813d423688f88356e958f9893852c4af36528
Bryan Hernandez <brayannhc07@gmail.com>
2021-12-01 12:36:34 -0600

    Seccion 12: Fin seccion, google signin

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,7 +4,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth.controller');
+const { login, googleSignIn } = require('../controllers/auth.controller');
 const { validarCampos } = require('../middlewares/validar-campo');
 
 const router = Router();
@@ -14,5 +14,10 @@ router.post('/', [
 	check('password', 'La contraseña es obligatoria.').not().isEmpty(),
 	validarCampos
 ], login);
+
+router.post('/google', [
+	check('token', 'El token de google es obligatorio.').not().isEmpty(),
+	validarCampos
+], googleSignIn);
 
 module.exports = router;
